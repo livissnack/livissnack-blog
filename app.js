4bfa8ddccc0fd2513f241e8419f8a2f5498b99d6
bruce <=>
2022-02-18 10:17:26 +0800

    submit

--- app.js
+++ app.js
@@ -3,14 +3,14 @@
 "use strict";
 
 const { exec } = require("child_process");
-const { apps } = require('./ecosystem.config')
-const node_env = process.env.NODE_ENV
-let run_port = 4000
+const { apps } = require("./ecosystem.config");
+const node_env = process.env.NODE_ENV;
+let run_port = 4000;
 
-if(node_env == 'production') {
-    run_port = apps[0].env_production.PORT
+if (node_env == "production") {
+  run_port = apps[0].env_production.PORT;
 } else {
-    run_port = apps[0].env.PORT
+  run_port = apps[0].env.PORT;
 }
 
 exec(`hexo server -p ${run_port} -s`, (error, stdout, stderr) => {
@@ -21,4 +21,4 @@ exec(`hexo server -p ${run_port} -s`, (error, stdout, stderr) => {
   console.log("stdout: ${stdout}");
   console.log("stderr: ${stderr}");
 });
-console.log('success load hexo server');
+console.log("success load hexo server");
